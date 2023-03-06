91f491b988dacecba8e36963f8e95aa96a61bd52
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-03-06 13:28:12 -0500

    Forms template and reactive

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  text: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SidemenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SidemenuComponent {
+  templateMenu: MenuItem[] = [
+    {
+      text: 'Basics',
+      route: './template/basics',
+    },
+    {
+      text: 'Dynamics',
+      route: './template/dynamics',
+    },
+    {
+      text: 'Switches',
+      route: './template/switches',
+    },
+  ];
+  reactiveMenu: MenuItem[] = [
+    {
+      text: 'Basics',
+      route: './reactive/basics',
+    },
+    {
+      text: 'Dynamics',
+      route: './reactive/dynamics',
+    },
+    {
+      text: 'Switches',
+      route: './reactive/switches',
+    },
+  ];
 }
--- src/app/template/basics/basics.component.ts
+++ src/app/template/basics/basics.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicsComponent implements OnInit {
+  @ViewChild('myForm') myForm!: NgForm;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  nameValid() {
+    return (
+      this.myForm?.controls['product']?.invalid &&
+      this.myForm?.controls['product']?.touched
+    );
   }
 
+  priceValid(): boolean{
+    return (
+      this.myForm?.controls['price']?.touched &&
+      this.myForm?.controls['price']?.value < 0
+    )
+  }
+
+  save() {
+    console.log(this.myForm);
+  }
 }
