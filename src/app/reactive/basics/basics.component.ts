afa9a33f82a7f14cd642b5606eb792f27aec833e
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-03-07 18:27:25 -0500

    reactive forms, formsBuilder, Validator

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicsComponent implements OnInit {
+  // myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('Rtx 4080ti'),
+  //   price: new FormControl(1500),
+  //   stocks: new FormControl(5),
+  // });
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    name: [, [Validators.required, Validators.minLength(3)]],
+    price: [, [Validators.required, Validators.min(0)]],
+    stocks: [, [Validators.required, Validators.min(0)]],
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm.reset({
+      name: 'Rtx 3080ti',
+      price: 1234,
+      stocks: 3,
+    });
   }
 
+  fiedlIsValid(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
--- src/app/reactive/dynamics/dynamics.component.ts
+++ src/app/reactive/dynamics/dynamics.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DynamicsComponent implements OnInit {
+export class DynamicsComponent {
+  myForm: FormGroup = this.fb.group({
+    name: ['Torito', [Validators.required, Validators.minLength(3)]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  fieldIsValid(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
   }
 
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
