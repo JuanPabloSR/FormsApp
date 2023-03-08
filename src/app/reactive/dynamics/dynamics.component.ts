bbefe0c2ea985f3025fc1f4f74f004475d4cf3a4
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-03-08 12:08:23 -0500

    reactive forms dynamics

--- src/app/reactive/dynamics/dynamics.component.ts
+++ src/app/reactive/dynamics/dynamics.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
@@ -9,7 +15,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DynamicsComponent {
   myForm: FormGroup = this.fb.group({
     name: ['Torito', [Validators.required, Validators.minLength(3)]],
+    favorites: this.fb.array(
+      [
+        ['God of war', Validators.required],
+        ['Dbz budokai', Validators.required],
+      ],
+      Validators.required
+    ),
   });
+
+  newFavorite: FormControl = this.fb.control('', Validators.required);
+
+  get favoritesArray() {
+    return this.myForm.get('favorites') as FormArray;
+  }
 
   constructor(private fb: FormBuilder) {}
 
@@ -17,6 +36,18 @@ export class DynamicsComponent {
     return (
       this.myForm.controls[field].errors && this.myForm.controls[field].touched
     );
+  }
+
+  addFavorite() {
+    if (this.newFavorite.invalid) {
+      return;
+    }
+    // this.favoritesArray.push(new FormControl(this.newFavorite.value, Validators.required));á
+    this.favoritesArray.push(
+      this.fb.control(this.newFavorite.value, Validators.required)
+    );
+
+    this.newFavorite.reset();
   }
 
   save() {
